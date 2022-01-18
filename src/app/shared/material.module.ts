b16e4ae32819c05fc5c5b5aabad48ab5b031badb
gabesclh <gabestylik@gmail.com>
2022-01-18 11:58:45 -0300

    feat: redução nos imports do material

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,20 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -22,31 +15,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     CommonModule,
     MatButtonModule,
-    MatDividerModule,
     MatIconModule,
-    MatToolbarModule,
     MatSnackBarModule,
     MatFormFieldModule,
     MatInputModule,
     MatRippleModule,
     MatExpansionModule,
-    MatCardModule,
-    MatProgressBarModule,
     MatSelectModule,
   ],
   exports: [
     CommonModule,
     MatButtonModule,
-    MatDividerModule,
     MatIconModule,
-    MatToolbarModule,
     MatSnackBarModule,
     MatFormFieldModule,
     MatInputModule,
     MatRippleModule,
     MatExpansionModule,
-    MatCardModule,
-    MatProgressBarModule,
     MatSelectModule,
   ]
 })
