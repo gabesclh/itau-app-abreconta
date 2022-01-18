911d1dfd20e426d453da6f816dc3eea88e5efa2b
gabesclh <gabestylik@gmail.com>
2022-01-18 12:02:55 -0300

    feat: removendo imports desnecessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire/compat/';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JwtModule } from '@auth0/angular-jwt';
 import { IConfig, NgxMaskModule } from 'ngx-mask';
-import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PerfilModule } from './perfil/perfil.module';
@@ -28,16 +24,6 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    JwtModule.forRoot({
-      config: {
-        // tokenGetter: tokenGetter,
-        allowedDomains: ['localhost:8080', 'api.colls.co'],
-        disallowedRoutes: ['http://example.com/examplebadroute/'],
-      },
-    }),
     NgxMaskModule.forRoot(),
   ],
   exports: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyAqVeWfXHA9Gv7HVTtl1DuJrjkGZI0oONs",
-    authDomain: "itau-app-abreconta.firebaseapp.com",
-    projectId: "itau-app-abreconta",
-    storageBucket: "itau-app-abreconta.appspot.com",
-    messagingSenderId: "378511918797",
-    appId: "1:378511918797:web:7460df2a8321cf92184b84",
-    measurementId: "G-K5F33EKF9Z"
-  },
+  // firebase: {
+  //   apiKey: "AIzaSyAqVeWfXHA9Gv7HVTtl1DuJrjkGZI0oONs",
+  //   authDomain: "itau-app-abreconta.firebaseapp.com",
+  //   projectId: "itau-app-abreconta",
+  //   storageBucket: "itau-app-abreconta.appspot.com",
+  //   messagingSenderId: "378511918797",
+  //   appId: "1:378511918797:web:7460df2a8321cf92184b84",
+  //   measurementId: "G-K5F33EKF9Z"
+  // },
   apiUrl: 'http://localhost:8083/usuario/',
 };
 
